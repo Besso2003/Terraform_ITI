ad0accc26c431238f3eaa2f69e26305b1c75363c
Besso2003 <bassant.ali23@icloud.com>
2026-05-12 23:08:48 +0300

    Edited Backend Configuration for Terraform State Management

--- Labs/backend.tf
+++ Labs/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "bassant-tf-state"
+    bucket         = "bassant-tf-state-v2"
     key            = "terraform/state.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-lock-table"
