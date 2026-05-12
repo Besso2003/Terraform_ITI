951f5762182db698036407157160df7d4d46d8f6
Besso2003 <bassant.ali23@icloud.com>
2026-05-12 23:59:08 +0300

    edited: trigger_notification.tf

--- Labs/trigger_notification.tf
+++ Labs/trigger_notification.tf
@@ -3,11 +3,11 @@ resource "aws_lambda_permission" "allow_s3" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.send_email.function_name
   principal     = "s3.amazonaws.com"
-  source_arn    = "arn:aws:s3:::bassant-tf-state"
+  source_arn    = "arn:aws:s3:::bassant-tf-state-v2"
 }
 
 resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket = "bassant-tf-state"
+  bucket = "bassant-tf-state-v2"
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.send_email.arn
