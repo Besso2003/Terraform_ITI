9f6e2b597775b7b74158de68780fe50585287acb
Besso2003 <bassant.ali23@icloud.com>
2026-05-20 19:53:00 +0300

    made sure the Ansible folder exists before creating inventory and ssh config files, and added the new files to the commit message.

--- Terraform/ansible_inventory.tf
+++ Terraform/ansible_inventory.tf
@@ -1,16 +1,28 @@
+resource "null_resource" "create_ansible_dir" {
+  provisioner "local-exec" {
+    command = "mkdir -p ${path.module}/../Ansible"
+  }
+}
+
 resource "local_file" "ansible_inventory" {
+  depends_on = [null_resource.create_ansible_dir]
+
   content = templatefile("${path.module}/templates/inventory.tpl", {
     bastion_ip = aws_instance.bastion.public_ip
     app_ip     = aws_instance.app.private_ip
   })
+
   filename        = "${path.module}/../Ansible/inventory.ini"
   file_permission = "0644"
 }
 
 resource "local_file" "ssh_config" {
+  depends_on = [null_resource.create_ansible_dir]
+
   content = templatefile("${path.module}/templates/ssh_cfg.tpl", {
     bastion_ip = aws_instance.bastion.public_ip
   })
+
   filename        = "${path.module}/../Ansible/ssh.cfg"
   file_permission = "0600"
 }
