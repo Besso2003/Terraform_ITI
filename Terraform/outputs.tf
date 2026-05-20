6d8f72a45868318efb48d34d20b3e516b17b0388
Besso2003 <bassant.ali23@icloud.com>
2026-05-20 17:53:11 +0300

    Output the bastion public IP and app private IP in the outputs.tf file.

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -13,3 +13,10 @@ output "environment" {
 output "alb_dns_name" {
   value = aws_lb.app.dns_name
 }
+output "bastion_public_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+output "app_private_ip" {
+  value = aws_instance.app.private_ip
+}
